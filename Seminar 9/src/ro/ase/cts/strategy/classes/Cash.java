ed68ec031ecf1b72ab83bdedc9d616cfec067f68
LENOVO <LENOVO@DESKTOP-UG1T25U>
2021-04-22 08:03:20 +0300

    First commit

---
+++ Seminar 9/src/ro/ase/cts/strategy/classes/Cash.java
@@ -0,0 +1,12 @@
+package ro.ase.cts.strategy.classes;
+
+
+public class Cash implements ModPlata {
+
+	@Override
+	public void realizeazaPlata(float suma) {
+		// TODO Auto-generated method stub
+		System.out.println("Se plateste cash suma de: " + suma);
+	}
+
+}
---
+++ Seminar 9/src/ro/ase/cts/strategy/main/Program.java
@@ -0,0 +1,19 @@
+package ro.ase.cts.strategy.main;
+
+import ro.ase.cts.strategy.classes.Card;
+import ro.ase.cts.strategy.classes.Cash;
+
+import ro.ase.cts.strategy.classes.Client;
+public class Program {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Client client = new Client("Dani");
+		client.setModPlata(new Cash());
+		client.platesteNota(200.50f);
+		
+		client.setModPlata(new Card(1000f));
+		client.platesteNota(500);
+		client.platesteNota(501);
+	}
+}
